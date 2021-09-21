86bcbf4870f79e80cf1fd9e5aa4d2a1a172db903
Andre-Ceschia <andre.ceschia04@gmail.com>
2021-09-20 20:07:22 -0400

    commit

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author_email="andre.ceschia04@gmail.com",
     description=DESCRIPTION,
     packages=find_packages(),
-    install_requires=['cloudscrapper', 'bs4'],
+    install_requires=['cloudscraper', 'bs4'],
     keywords=['python', 'finance', 'wealthsimple', 'wealthsimple trade', 'broker api'],
     classifiers=[
         "Development Status :: 4 - Beta",
